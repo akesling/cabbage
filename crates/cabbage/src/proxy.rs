039738030a38e990c7f57d72c3cbd62710eacfd8
Kyle Beserra <kyle@emptysetmath.com>
2025-07-30 15:31:54 -0400

    dropped separate tasks

--- crates/cabbage/src/proxy.rs
+++ crates/cabbage/src/proxy.rs
@@ -2,7 +2,7 @@ use futures_util::SinkExt;
 use redis_protocol::codec::Resp2;
 use tokio::net::TcpStream;
 use tokio_stream::StreamExt;
-use tokio_util::codec::{FramedRead, FramedWrite};
+use tokio_util::codec::Framed;
 
 pub async fn handle_connection(
     client_socket: TcpStream,
@@ -12,83 +12,62 @@ pub async fn handle_connection(
     let target_socket = TcpStream::connect(&target_addr).await?;
     log::info!("connected with target at: {}", target_addr);
 
-    let (client_read, client_write) = client_socket.into_split();
-    let (target_read, target_write) = target_socket.into_split();
+    let mut client_framed = Framed::new(client_socket, Resp2::default());
+    let mut target_framed = Framed::new(target_socket, Resp2::default());
 
-    let client_reader = FramedRead::new(client_read, Resp2::default());
-    let client_writer = FramedWrite::new(client_write, Resp2::default());
-    let target_reader = FramedRead::new(target_read, Resp2::default());
-    let target_writer = FramedWrite::new(target_write, Resp2::default());
+    let mut command_count = 0u64;
+    let mut response_count = 0u64;
 
-    let client_to_target = tokio::spawn(forward_frames(
-        client_reader,
-        target_writer,
-        "Client -> Target",
-        "command",
-    ));
+    loop {
+        tokio::select! {
+            // Handle frames from client -> target
+            frame_result = client_framed.next() => {
+                match frame_result {
+                    Some(Ok(frame)) => {
+                        command_count += 1;
+                        log::info!("Client -> Target: command #{} - {:?}", command_count, frame);
 
-    let target_to_client = tokio::spawn(forward_frames(
-        target_reader,
-        client_writer,
-        "Target -> Client",
-        "response",
-    ));
-
-    let (client_result, target_result) = tokio::join!(client_to_target, target_to_client);
-
-    if let Err(e) = client_result.and_then(|r| Ok(r)) {
-        log::error!("Client to target error: {}", e);
-    }
-
-    if let Err(e) = target_result.and_then(|r| Ok(r)) {
-        log::error!("Target to client error: {}", e);
-    }
-
-    log::info!("Connection closed");
-    Ok(())
-}
-
-async fn forward_frames<R, W>(
-    mut reader: FramedRead<R, Resp2>,
-    mut writer: FramedWrite<W, Resp2>,
-    direction: &str,
-    frame_type: &str,
-) -> anyhow::Result<()>
-where
-    R: tokio::io::AsyncRead + Unpin,
-    W: tokio::io::AsyncWrite + Unpin,
-{
-    let mut frame_count = 0u64;
-
-    while let Some(frame_result) = reader.next().await {
-        match frame_result {
-            Ok(frame) => {
-                frame_count += 1;
-                log::info!(
-                    "{}: {} #{} - {:?}",
-                    direction,
-                    frame_type,
-                    frame_count,
-                    frame
-                );
-
-                if let Err(e) = writer.send(frame).await {
-                    log::error!("Failed to send {}: {}", frame_type, e);
-                    break;
+                        if let Err(e) = target_framed.send(frame).await {
+                            log::error!("Failed to send command to target: {}", e);
+                            break;
+                        }
+                    }
+                    Some(Err(e)) => {
+                        log::error!("Error reading from client: {}", e);
+                        break;
+                    }
+                    None => {
+                        log::info!("Client connection closed");
+                        break;
+                    }
                 }
             }
-            Err(e) => {
-                log::error!("Error reading {}: {}", frame_type, e);
-                break;
+
+            // Handle frames from target -> client
+            frame_result = target_framed.next() => {
+                match frame_result {
+                    Some(Ok(frame)) => {
+                        response_count += 1;
+                        log::info!("Target -> Client: response #{} - {:?}", response_count, frame);
+
+                        if let Err(e) = client_framed.send(frame).await {
+                            log::error!("Failed to send response to client: {}", e);
+                            break;
+                        }
+                    }
+                    Some(Err(e)) => {
+                        log::error!("Error reading from target: {}", e);
+                        break;
+                    }
+                    None => {
+                        log::info!("Target connection closed");
+                        break;
+                    }
+                }
             }
         }
     }
 
-    log::info!(
-        "{} finished after {} {}s",
-        direction,
-        frame_count,
-        frame_type
-    );
+    log::info!("Connection closed");
     Ok(())
 }
